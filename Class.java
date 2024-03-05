653e966ae671d662b9ac78fa2701cb0f07ecfeb6
Dheeravathshilpa <126770868+Dheeravathshilpa@users.noreply.github.com>
2024-03-05 21:54:03 +0530

    Project 1

---
+++ Class.java
@@ -0,0 +1,17 @@
+package com.si.day2;
+
+public class Class {
+	int a=10;
+	static int b=20;
+public static void main(String[] args) {
+	int c=30;
+	Class obj=new Class();
+	System.out.println(obj.a);
+	System.out.println(Class.b);
+	System.out.println(b);
+	System.out.println(c);
+	
+	
+}
+
+}
